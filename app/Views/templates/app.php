1c56fcee63f12c9c46bc63fca07af8f7e37d9fed
JBA19 <jbacorrtech@gmail.com>
2021-04-27 13:22:51 +0200

    MAJ 27/04

--- app/Views/templates/app.php
+++ app/Views/templates/app.php
@@ -12,7 +12,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="Au petit creux sympa" content="Restauration rapide à base de produit locaux situé à CHAMBERET en Corrèze. Ouvert le midi et le soir du jeudi au dimanche. Commande par téléphone.">
+        <meta name="description" content="Restauration rapide à base de produit locaux situé à CHAMBERET en Corrèze. Ouvert le midi et le soir du jeudi au dimanche. Commande par téléphone.">
         <title>Au petit creux sympa</title>
         <link rel="shortcut icon" href="<?php echo base_url(); ?>/favicon.ico" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
